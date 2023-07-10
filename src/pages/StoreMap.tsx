3f7573fea48fc55a98f503441b6df46383747050
eun6 <yy.eun6@gmail.com>
2023-07-10 23:44:18 +0900

    [feat] 선호 지역 필터 틀 작성

--- src/pages/StoreMap.tsx
+++ src/pages/StoreMap.tsx
@@ -13,6 +13,7 @@ import NaverMapView, {Marker, Path} from 'react-native-nmap';
 import Filter1 from '../components/Filter1';
 import Filter2 from '../components/Filter2';
 import Filter3 from '../components/Filter3';
+import Filter4 from '../components/Filter4';
 
 function StoreMap() {
   const [latitude, setLatitude] = useState(37.56667);
@@ -20,6 +21,7 @@ function StoreMap() {
   const [checkvisible, setCheckVisible] = useState(false);
   const [categoryVisible, setCategoryVisible] = useState(false);
   const [liquorVisible, setLiquorVisible] = useState(false);
+  const [favorLocation, setFavorLocation] = useState(false);
 
   return (
     <View>
@@ -31,7 +33,7 @@ function StoreMap() {
         }}>
         <View style={{flexDirection: 'row'}}>
           <TextInput
-            style={{backgroundColor: 'pink', flex: 1}}
+            style={{backgroundColor: '#F2F2F2', flex: 1}}
             placeholder="검색할 내용을 입력해 주세요. 예: 강남 시티뷰가 좋은 집"
             //onChangeText={(text): void => setSearchWord(text)}
           />
@@ -73,11 +75,14 @@ function StoreMap() {
           <Modal visible={liquorVisible} transparent statusBarTranslucent>
             <Filter3 setLiquorVisible={setLiquorVisible} />
           </Modal>
-          <Button title="선호 지역" onPress={() => Alert.alert('선호 지역')} />
+          <Button title="선호 지역" onPress={() => setFavorLocation(true)} />
+          <Modal visible={favorLocation} transparent statusBarTranslucent>
+            <Filter4 setFavorLocation={setFavorLocation} />
+          </Modal>
         </View>
         <View style={styles.reservation}>
-          <Text>예약 현황</Text>
-          <Text> {`>`} </Text>
+          <Text style={{color: 'white'}}>예약 현황</Text>
+          <Text style={{color: 'white'}}> {`>`} </Text>
         </View>
         <NaverMapView
           style={{width: '100%', height: '100%'}}
@@ -111,8 +116,9 @@ const styles = StyleSheet.create({
   },
   reservation: {
     flexDirection: 'row',
-    backgroundColor: 'green',
+    backgroundColor: '#4E6D5E',
     justifyContent: 'flex-end',
+    padding: 5,
   },
   currentStatus: {
     position: 'absolute',
