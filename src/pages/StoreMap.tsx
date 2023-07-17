85825c2b3b3a730443c3ee4b1923801b47d60c65
eun6 <yy.eun6@gmail.com>
2023-07-17 10:31:55 +0900

    [feat] 주점 위치 정보 상태관리 추가

--- src/pages/StoreMap.tsx
+++ src/pages/StoreMap.tsx
@@ -17,16 +17,29 @@ import Filter2 from '../components/Filter2';
 import Filter3 from '../components/Filter3';
 import Filter4 from '../components/Filter4';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useAppDispatch} from '../store';
+import storeMapSlice from '../slices/storeMap';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
 
 function StoreMap() {
+  const dispatch = useAppDispatch();
+  const accessToken = 'accessToken';
+  //useSelector((state: RootState) => state.user.accessToken);
+  const storeList = useSelector(
+    (state: RootState) => state.storeMap.defaultStores,
+  );
+  const searchStoreList = useSelector(
+    (state: RootState) => state.storeMap.searchStores,
+  );
+
   //현재 위치
   const [myPosition, setMyPosition] = useState<{
     latitude: number;
     longitude: number;
   } | null>(null);
-
-  //모든 주점 위치 ▶ markers에 초기 데이터 값 안 들어가고 있음. 추후 확인.
-  const [markers, setMarkers] = useState(storeData);
 
   //필터링
   const [checkvisible, setCheckVisible] = useState(false);
@@ -34,11 +47,13 @@ function StoreMap() {
   const [liquorVisible, setLiquorVisible] = useState(false);
   const [favorLocation, setFavorLocation] = useState(false);
 
+  //모든 주점 위치 ▶ markers에 초기 데이터 값 안 들어가고 있음. 추후 확인.
+  const [markers, setMarkers] = useState('');
   //검색창
   const [searchWord, setSearchWord] = useState('');
 
+  //현재 위치 정보 불러오기
   useEffect(() => {
-    //현재 위치 정보 불러오기
     Geolocation.getCurrentPosition(
       info => {
         setMyPosition({
@@ -54,6 +69,51 @@ function StoreMap() {
       },
     );
   }, []);
+
+  //사용자 위치정보 기반 등록된 지점 보여주기
+  useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+    const loadStore = async () => {
+      try {
+        // const response = await axios.post(`${Config.API_URL}/store`, {
+        //   //request 정보
+        //   headers: {authorization: `Bearer ${accessToken}`},
+        // });
+        // console.log(response);
+
+        //더미 데이터
+        const response = [
+          {
+            restaurant_id: 1,
+            restaurant_name: '테스트1',
+            latitude: 37.497175,
+            longitude: 127.029099,
+          },
+          {
+            restaurant_id: 2,
+            restaurant_name: '테스트2',
+            latitude: 37.497175,
+            longitude: 127.027926,
+          },
+        ];
+        dispatch(storeMapSlice.actions.setStore(response));
+        console.log('storeList입니다!!!', storeList);
+        setMarkers(storeData);
+      } catch (error) {
+        console.error(error);
+        if ((error as AxiosError).response?.data.code === '400') {
+          Alert.alert(
+            '알림',
+            '가게 정보를 불러오지 못했습니다. 통신 상태 확인 후 다시 한번 시도해주세요.',
+          );
+        }
+      }
+    };
+    loadStore();
+  }, [dispatch]);
+
   if (!myPosition || !myPosition.latitude) {
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
@@ -61,36 +121,61 @@ function StoreMap() {
       </View>
     );
   }
-  //더미 데이터
-  const data = [
-    {
-      id: 1,
-      title: '테스트1',
-      coordinate: {latitude: 37.497175, longitude: 127.029099},
-    },
-    {
-      id: 2,
-      title: '테스트2',
-      coordinate: {latitude: 37.497175, longitude: 127.027926},
-    },
-  ];
-  const searchData = [
-    {
-      id: 1,
-      title: '서칭1',
-      coordinate: {latitude: 37.497175, longitude: 127.027299},
-    },
-    {
-      id: 2,
-      title: '서칭2',
-      coordinate: {latitude: 37.497175, longitude: 127.027926},
-    },
-  ];
-  const storeData = data.map(store => {
+
+  const searchStore = async () => {
+    try {
+      // const response = await axios.post(`${Config.API_URL}/store`, {
+      //   //request 정보
+      //   headers: {authorization: `Bearer ${accessToken}`},
+      // });
+      // console.log(response);
+
+      //더미 데이터
+      const response = [
+        {
+          restaurant_id: 1,
+          restaurant_name: '서칭1',
+          latitude: 37.497175,
+          longitude: 127.027299,
+        },
+        {
+          restaurant_id: 2,
+          restaurant_name: '서칭2',
+          latitude: 37.497175,
+          longitude: 127.027926,
+        },
+      ];
+      dispatch(storeMapSlice.actions.setSearchStore(response));
+      console.log('~~~~~~searchStoreList!!!', searchStoreList);
+      const searchData = searchStoreList[0].map((store: any) => {
+        return {
+          id: store.restaurant_id,
+          title: store.restaurant_name,
+          coordinate: {
+            latitude: store.latitude,
+            longitude: store.longitude,
+          },
+        };
+      });
+      console.log('~~~~~~ searchData입니다', searchData);
+      setMarkers(searchData);
+    } catch (error) {
+      console.error(error);
+      if ((error as AxiosError).response?.data.code === '400') {
+        Alert.alert(
+          '알림',
+          '가게 정보를 불러오지 못했습니다. 통신 상태 확인 후 다시 한번 시도해주세요.',
+        );
+      }
+    }
+  };
+
+  //마커 형식에 맞게 data 매핑해서 받아옴.
+  const storeData = storeList[0].map((store: any) => {
     return {
-      id: store.id,
-      title: store.title,
-      coordinate: store.coordinate,
+      id: store.restaurant_id,
+      title: store.restaurant_name,
+      coordinate: {latitude: store.latitude, longitude: store.longitude},
     };
   });
 
@@ -108,14 +193,7 @@ function StoreMap() {
           <Pressable
             style={{flex: 1}}
             onPress={(): void => {
-              const searchStore = searchData.map((store: any) => {
-                return {
-                  id: store.id,
-                  title: store.title,
-                  coordinate: store.coordinate,
-                };
-              });
-              setMarkers(searchStore);
+              searchStore();
               // axiosInstance
               //   .post('houses/search', {
               //     searchWord,
@@ -181,6 +259,8 @@ function StoreMap() {
             latitude: myPosition.latitude,
             longitude: myPosition.longitude,
           }}>
+          {console.log('storeData입니다', storeData)}
+          {console.log('==============markerData', markers)}
           {markers &&
             markers.map(
               (store: {
